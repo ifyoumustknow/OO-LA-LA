1e4016c3b457df291c2415a7f16c35f17d860870
Rob Tang <ifyoumustknow@gmail.com>
2019-11-19 09:31:25 -0800

    revised story

--- magic.js
+++ magic.js
@@ -200,12 +200,12 @@ The End.<p></div>
             $("#body3").css('display', 'inline');
             $("#body3").toggleClass("fade-in");
             $("#body3").append(`<div><h1>Times Up!</h1></div> 
-           <p>You just woken up with a blinding white light above you. You look to your left and see what appears to be something shiny, like the silverware set from Christmas dinner. Your head is throbbing. </p>
+           <p>You have just woken up by the blinding white light above you. You look to your left and see what appears to be something shiny, like the silverware set from Christmas dinner. Your head is throbbing. </p>
            <p>You drop back down.</p>
            <p> The last thing you remember was being in the UC Bootcamp class and then leaving, then walking to your car and then it all went dark.</p>
            <p> You rub your eyes and try to sit up. And when you finally do, you realize you're on some sort of table - a cold metallic table, the kind used for operations or seen in morgues.</P>
            <p>This can't be good.</p> 
-           <p>You try to get off the table, but instead fall off and land on your knees instead. The pain is excruciating, you try to scream, but instead you begin to vomit violently. Your body begins to shake and you tuck yourself into a tight ball. Once the episode subsides you pull yourself up by the table.</p><p>Across from the table next to the Christmas utensils, you see a medicine bottle. You reach for it, hoping it's a remedy, and the label reads:</p>
+           <p>You try to get off the table, but instead fall onto your knees. The pain is excruciating, you try to scream, but you vomit violently. Your body start to shake and you tuck yourself into a tight ball. Once the episode subsides you pull yourself up by the table.</p><p>Across from the table next to the Christmas utensils, you see a medicine bottle. You reach for it, hoping it's a remedy, and the label reads:</p>
 
           <div style="font-family: 'IM Fell English SC' , serif ; background-color:wheat ; width:50vw; height:auto; color:black; padding:5%;"><h5>Tetraethylammonium[31]</h5><p>Poison: if vomiting, muscle weakness followed by convulsions and cold sweat, seek immediate medical attention or symptoms will increase and may lead to death.
            </div>
@@ -213,7 +213,7 @@ The End.<p></div>
            <p>You now notice in the lower right corner of your vision a small timer. When did that appear? It looks like the timer in that game you played. Why is it counting down? What is it counting down to?</p>
            <p> The voice in your head, the one that keeps you away from stepping too close to the edge, gets louder and louder and telling you to get out.</p>
            <p>You pull yourself together, gather your strength and survey the room. </p>
-           <p>The room is about 20' by 20', lit by the one light above. You can vaguely see a door on the far wall. There's also a large tub in the back of the room. The floor is tiled and there is what looks like a semi-large drainage gate under the table. </p>
+           <p>The room is about 20' by 20', lit by the one light above. You can vaguely see a door on the far wall. The floor is tiled and there is what looks like a semi-large drainage gate under the table. </p>
            <p> The voice tells you:</p> 
            <button id="getout3" class="btn btn-dark">Make for the door.</button>  <button id="getout4" class="btn btn-dark">Drainage Leads Out.</button>`)
 
@@ -233,7 +233,7 @@ The End.<p></div>
             $("#body3").toggleClass("fade-in");
             $("#body3").append(`<div><h1>The Door.</h1></div>
 
-           <p>You can feel the poison slowing your movement getting to the door and it takes you 3 minutes getting there.</p>
+           <p>You feel woozy and it takes you 3 minutes getting to the doors.</p>
            <p>The door appears to be made of metal. You place an ear trying to see if you can hear anything on the other side. You hear nothing. </p>
            <p>You turn the handle and relieved that it's not locked. You carefully open the door and step out. There's a set of metal stairs leading up and a set leading down.</p> 
            <p>You decide to take the stairs going:</p>
@@ -332,7 +332,7 @@ The End.<p></div>
             $("#body3").css('display', 'inline');
             $("#body3").toggleClass("fade-in");
             $("#body3").append(`<div><h1>Hallway Left</h1></div>
-           <p>At the bottom of the stairs there's a metal door that is marked "PARKING"</p>
+           <p>At the top of the stairs there's a metal door that is marked "PARKING"</p>
            <p>You slowly open the doo</p>
            <p> Which way now?</p>
            <button id="getout3" class="btn btn-dark">Right?</button>  <button id="stay3" class="btn btn-dark">Left?</button>  <button id="getout4" class="btn btn-dark">Back Up?</button>`)
@@ -342,7 +342,7 @@ The End.<p></div>
         });
 
         // GET OUT 8//Hallway Right.
-        $(document).on('click', '#getout7', function () {
+        $(document).on('click', '#getout8', function () {
             console.log("getting the fuck out")
             $("#story1").css('display', 'none');
             $("#story2").css('display', 'none');
@@ -371,7 +371,9 @@ The End.<p></div>
             $("#body3").empty()
             $("#body3").css('display', 'inline');
             $("#body3").toggleClass("fade-in");
-            $("#body3").append(`<div><h1>The Getaway</h1></div> <p>You get to your car and quickly realize that you don’t have your keys. A small memory snaps into view. You took your keys out before you left the elevator. And you heard a distinct clink on the ground before you fell. You retrace your steps from the elevator. You get to where you remember passing out and bend down onto your knees. The ground is dark and the poison in your system distorts your vision. You scuddle along the floor feeling for your keys and after 5 minutes you at last found them. You quickly get up and rush to your car. You jump into the driver seat and jam the key into the ignition and the car comes to life with no hesitation. You pull the car in reverse, and then drive towards the gate. The ticket machine is waiting patiently to accept your ticket. You search your pockets and check the visor. No ticket. There’s a call button on the machine. You pause for the moment and remember all those horror movies. What if the murderer is the attendant? You stare out and see only a gate preventing you from escape. With determination, you put the car in reverse and put enough distance between you and the gate. You put the car into drive and scream, “F***K this night!” and slam on the gas launching your car towards the closed gate. As you approach the gate at a high velocity, you somehow trigger the safety and the gate begins to open. You realize too late as you fly through the bottom of gate while the gate is still rising. The bottom of the gate barrels towards you and slams through the windshield, severing your car’s top half and simultaneously decapitating you.</p>
+            $("#body3").append(`<div><h1>The Getaway</h1></div> <p>You get to your car and quickly realize that you don’t have your keys. A small memory snaps into view. You took your keys out before you left the elevator. And you heard a distinct clink on the ground before you fell. </p>
+            <p>You retrace your steps from the elevator.</p>
+            <p>You get to where you remember passing out and bend down onto your knees. The ground is dark and the poison in your system distorts your vision. You scuddle along the floor feeling for your keys and after 5 minutes you at last found them.</p><p>You quickly get up and rush to your car. You jump into the driver seat and jam the key into the ignition and the car comes to life with no hesitation. You pull the car in reverse, and drive towards the gate. The ticket machine is waiting patiently to accept your ticket. You search your pockets and check the visor. No ticket. There’s a call button on the machine. You pause for the moment and remember all those horror movies. What if the murderer is the attendant? You stare out and see only a gate preventing you from escape. With determination, you put the car in reverse and put enough distance between you and the gate. You put the car into drive and scream, “F***K this night!” and slam on the gas launching your car towards the closed gate. As you approach the gate at a high velocity, you somehow trigger the safety and the gate begins to open. You realize too late as you fly through the bottom of gate while the gate is still rising. The bottom of the gate barrels towards you and slams through the windshield, severing your car’s top half and simultaneously decapitating you.</p>
 
 <p>You never make it to your next class.</p><br>
 <button id="startover" class="btn btn-dark">Start Over?</button>`)
